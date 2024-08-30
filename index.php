2ae29e9c62879a06224ab494ab576bf460d9f565
Gerardo <gerardomanuel10032003@gmail.com>
2024-08-30 18:31:02 -0500

    Index: Se mejoro la sintaxis

--- index.php
+++ index.php
@@ -5,31 +5,45 @@
         <section>
             <div class="carousel slide" data-bs-ride="carousel" id="carousel-1" style="height: 600px;">
                 <div class="carousel-inner h-100" style="color: var(--bs-body-bg);">
-                    <div class="carousel-item active h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" alt="Slide Image" src="assets/img/photo-1628191140023-d3b347041e2c.jpg" style="z-index: -1;">
+                    <div class="carousel-item active h-100">
+                        <img class="w-100 d-block position-absolute h-100 fit-cover" alt="Slide Image" src="assets/img/photo-1628191140023-d3b347041e2c.jpg" style="z-index: -1;">
+                        
                         <div class="container d-flex flex-column justify-content-center h-100">
                             <div class="row">
                                 <div class="col-md-6 col-xl-4 offset-md-2">
                                     <div style="max-width: 350px;">
-                                        <h1 class="text-uppercase fw-bold"><span style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">Aqui ira una frase con referencia a la empresa</span></h1>
+                                        <h1 class="text-uppercase fw-bold">
+                                            <span style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">
+                                                Aqui ira una frase con referencia a la empresa
+                                            </span>
+                                        </h1>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="carousel-item h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" alt="Slide Image" src="assets/img/photo-1627662168223-7df99068099a-1.jpg" style="z-index: -1;">
+                    <div class="carousel-item h-100">
+                        <img class="w-100 d-block position-absolute h-100 fit-cover" alt="Slide Image" src="assets/img/photo-1627662168223-7df99068099a-1.jpg" style="z-index: -1;">
+                        
                         <div class="container d-flex flex-column justify-content-center h-100">
                             <div class="row">
                                 <div class="col-md-6 col-xl-4 offset-md-2">
                                     <div style="max-width: 350px;">
-                                        <h1 class="text-uppercase fw-bold"><span style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">Aqui ira una frase con referencia a la empresa</span></h1>
+                                        <h1 class="text-uppercase fw-bold">
+                                            <span style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">
+                                                Aqui ira una frase con referencia a la empresa
+                                            </span>
+                                        </h1>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="carousel-item h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" alt="Slide Image" src="assets/img/photo-1628191078376-f31a36c9f2cf.jpg" style="z-index: -1;">
+                    <div class="carousel-item h-100">
+                        <img class="w-100 d-block position-absolute h-100 fit-cover" alt="Slide Image" src="assets/img/photo-1628191078376-f31a36c9f2cf.jpg" style="z-index: -1;">
+                        
                         <div class="container d-flex flex-column justify-content-center h-100">
                             <div class="row">
                                 <div class="col-md-6 col-xl-4 offset-md-2">
@@ -48,7 +62,8 @@
                         <span class="visually-hidden">Previous</span>
                     </a>
                     <a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next">
-                        <span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span>
+                        <span class="carousel-control-next-icon"></span>
+                        <span class="visually-hidden">Next</span>
                     </a>
                 </div>
 

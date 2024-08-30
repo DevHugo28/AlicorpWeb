cfbb2f5845e7e63e6aa895f3f9166f4b27604050
Gerardo <gerardomanuel10032003@gmail.com>
2024-08-30 17:52:53 -0500

    Creación Navbar_Footer

---
+++ navbar_footer/navbar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alicorp</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-icons.css">
+    <link rel="stylesheet" href="assets/css/Hero-Carousel-images.css">
+    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider-swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+</head>
+<body>
+    <!-- navbar.php -->
+    <nav class="navbar navbar-expand-lg sticky-top bg-body py-3" style="border-bottom-style: solid; border-bottom-color: rgb(143,143,143);">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="index.php">
+                <span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon" style="background: rgb(255,0,0);">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-basket3-fill">
+                        <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.468 15.426.943 9h14.114l-1.525 6.426a.75.75 0 0 1-.729.574H3.197a.75.75 0 0 1-.73-.574z"></path>
+                    </svg>
+                </span>
+                <img src="assets/img/Alicorp-Logo.wine.svg" style="height: 30px;width: 100px;">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-despegable" aria-controls="navbar-despegable" aria-expanded="false" aria-label="Cambiar navegación">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbar-despegable">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="sobre_nosotros.php">Sobre Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contactenos.php">Contáctenos</a></li>
+                </ul>
+                <a class="btn btn-primary" role="button" href="login.php" style="background-color: rgb(30, 230, 30);">Iniciar Sesión</a>
+            </div>
+        </div>
+    </nav>

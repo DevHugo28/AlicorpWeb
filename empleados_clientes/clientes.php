e455de0829248bd579ed50a01994c4f7332ffa15
Gerardo <gerardomanuel10032003@gmail.com>
2024-08-30 18:21:37 -0500

    Creación: Distribución de carpetas PHP

---
+++ empleados_clientes/clientes.php
@@ -0,0 +1,10 @@
+    <?php include 'navbar_footer\navbar.php'; ?>
+
+    <!-- Aquí va el contenido de la página -->
+    <main>
+        <section>
+            
+        </section>
+    </main>
+
+    <?php include 'navbar_footer\footer.php'; ?>
